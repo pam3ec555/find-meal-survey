e1afb4785710df4327ae4fb2b5b7793043c007e8
Ramil Rakhmatullin <rep555333@gmail.com>
2024-05-18 11:02:02 +0700

    Fixed image error

--- src/steps/FinalStep.tsx
+++ src/steps/FinalStep.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import googleIcon from '../assets/google-icon.svg';
+import appleIcon from '../assets/apple-icon.svg';
 
 const FinalStep: FC = () => {
   return (
@@ -7,11 +9,11 @@ const FinalStep: FC = () => {
       <p>Solve the problem of finding places to eat once and for all.</p>
       <div style={{ display: 'flex' }}>
         <a href="https://apps.apple.com/th/app/vision-craft-ai/id6476929202" target="_blank" rel="noreferrer">
-          <img src="/ios-download.svg" alt="" />
+          <img src={appleIcon} alt="" />
         </a>
         <div style={{ width: `10px` }}></div>
         <a href="https://play.google.com/store/apps/details?id=com.example.app" target="_blank" rel="noreferrer">
-          <img src="/android-download.svg" alt="" />
+          <img src={googleIcon} alt="" />
         </a>
       </div>
     </div>
