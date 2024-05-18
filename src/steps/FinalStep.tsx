f49933d062ac919ff94cb9cbcdb598fb74c4900f
Ramil Rakhmatullin <rep555333@gmail.com>
2024-05-18 11:04:43 +0700

    Added posthog to download btns

--- src/steps/FinalStep.tsx
+++ src/steps/FinalStep.tsx
@@ -1,18 +1,34 @@
 import { FC } from 'react';
 import googleIcon from '../assets/android-download.svg';
 import appleIcon from '../assets/ios-download.svg';
+import { usePostHog } from 'posthog-js/react';
 
 const FinalStep: FC = () => {
+  const posthog = usePostHog();
   return (
     <div className="download-page">
       <h1>Download our app</h1>
       <p>Solve the problem of finding places to eat once and for all.</p>
       <div style={{ display: 'flex' }}>
-        <a href="https://apps.apple.com/th/app/vision-craft-ai/id6476929202" target="_blank" rel="noreferrer">
+        <a
+          href="https://apps.apple.com/th/app/vision-craft-ai/id6476929202"
+          target="_blank"
+          rel="noreferrer"
+          onClick={() => {
+            posthog.capture('download_ios');
+          }}
+        >
           <img src={appleIcon} alt="" />
         </a>
         <div style={{ width: `10px` }}></div>
-        <a href="https://play.google.com/store/apps/details?id=com.example.app" target="_blank" rel="noreferrer">
+        <a
+          href="https://play.google.com/store/apps/details?id=com.example.app"
+          target="_blank"
+          rel="noreferrer"
+          onClick={() => {
+            posthog.capture('download_android');
+          }}
+        >
           <img src={googleIcon} alt="" />
         </a>
       </div>
